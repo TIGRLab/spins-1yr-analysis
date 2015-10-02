706615e555404873ca2c21da33ff0f3db73f3a9a
edickie <erin.w.dickie@gmail.com>
2015-10-02 16:59:14 -0400

    very rough start

--- ENIGMA_measure_plotting.R
+++ ENIGMA_measure_plotting.R
@@ -1,6 +1,8 @@
+library("ggplot2", lib.loc="/opt/quarantine/R-extras/3.1.1/build")
 
 ## read in the data
-subjects <- read.table("/projects/jdv/data/spins/1yr/list.csv", quote="\"")
+setwd("/projects/edickie/code/spins-1yr-analysis")
+subjects <- read.table("list.csv", quote="\"")
 LandRvolumes <- read.csv("/projects/edickie/analysis/freesurfer_QA/SPINS/LandRvolumes.csv")
 
 CorticalMeasuresENIGMA_ThickAvg <- read.csv("/projects/edickie/analysis/freesurfer_QA/SPINS/CorticalMeasuresENIGMA_ThickAvg.csv")
@@ -14,6 +16,7 @@ subjects$Dx[subjects$code=="x"]<-NA
 subjects$Dx <- factor(subjects$Dx)
 
 subjects$SubjID <- paste0(subjects$subid,'_01')
+subjects$site <- NA
 subjects$site[grep('CMH',as.character(subjects$SubjID))]<-"CMH"
 subjects$site[grep('ZHH',as.character(subjects$SubjID))]<-"ZHH"
 subjects$site[grep('MRC',as.character(subjects$SubjID))]<-"MRC"
@@ -21,3 +24,6 @@ subjects$site <- as.factor(subjects$site)
 
 
 volumes <- merge(subjects,LandRvolumes,by="SubjID")
+volumes <- subset(volumes, !is.na(volumes$Dx))
+ggplot(volumes, aes(x=site, y=LLatVent, shape=Dx, fill=Dx)) +
+  geom_boxplot(outlier.shape = NA) + geom_point(alpha = 0.5, position=position_jitterdodge(jitter.width=0.01))
