5eca035ede0dfe54970f2c1657f491cb0879f81d
edickie <erin.w.dickie@gmail.com>
2015-10-14 17:03:13 -0400

    more FA plotting

--- bin/ENIGMA_measure_plotting.R
+++ bin/ENIGMA_measure_plotting.R
@@ -64,27 +64,32 @@ subjects <- subset(subjects, !is.na(subjects$Dx))
 # coD$conf.int
 ttestpvals <- function(df, splitvar, ttestformula) {  
   myformula = formula(ttestformula)
+  yvar = gsub(' ','',(strsplit(ttestformula,'~')[[1]][1]))
+  xvar = gsub(' ','',(strsplit(ttestformula,'~')[[1]][2]))
   coD = do.call("rbind", 
                     lapply(split(df, df[ ,c(splitvar)]), 
-                           function(x) cohen.d(myformula,x,hedges.correction=TRUE)$estimate))
-  coDSE = do.call("rbind", 
-                lapply(split(df, df[ ,c(splitvar)]), 
-                       function(x) cohen.d(myformula,x,hedges.correction=TRUE)$var))
+                           function(x) cohen.d(myformula,x,hedges.correction=TRUE)))
   t.ttest = do.call("rbind", 
                     lapply(split(df, df[ ,c(splitvar)]), 
-                           function(x) t.test(myformula,x)$statistic))
-  df.ttest = do.call("rbind",
-                    lapply(split(df, df[ ,c(splitvar)]), 
-                           function(x) t.test(myformula,x)$parameter))
-  p.ttest = do.call("rbind",
-                    lapply(split(df, df[ ,c(splitvar)]), 
-                           function(x) t.test(myformula,x)$p.value))
+                           function(x) t.test(myformula,x)))
   n.ttest = do.call("rbind", lapply(split(df, df[ ,c(splitvar)]), nrow))
-  p.ttest = cbind(coD, coDSE,t.ttest, df.ttest, p.ttest, 
-                  p.adjust(p.ttest, method="fdr"), 
-                  p.adjust(p.ttest, method="bonferroni"),
+  p.ttest = cbind(coD[ ,c('estimate','var')],
+                  t.ttest[ ,c('statistic', 'parameter', 'p.value')], 
+                  p.adjust(t.ttest[ ,c('p.value')], method="fdr"), 
+                  p.adjust(t.ttest[ ,c('p.value')], method="bonferroni"),
                   n.ttest)
-  colnames(p.ttest) = c('cohen.D','cohenD.se','tstat','df',"raw", "fdr", "bonferroni","n")
+  mycolnames = c('cohen.D','cohenD.se','tstat','df',"raw", "fdr", "bonferroni","n")
+  
+  ## now iterate through and find mean and se for subgroups
+  for (iname in levels(df[ ,c(xvar)])) {
+    h_df = subset(df,df[ ,c(xvar)]==iname)
+    means = do.call("rbind", lapply(split(h_df[ ,c(yvar)], h_df[ ,c(splitvar)]), mean))
+    SE = do.call("rbind", lapply(split(h_df[ ,c(yvar)], h_df[ ,c(splitvar)]), function(x) sd(x)/sqrt(length(x)) ))
+    p.ttest = cbind(p.ttest, means, SE)
+    mycolnames = c(mycolnames,paste0(c('mean_','se_'),iname))
+  }
+  colnames(p.ttest) = mycolnames
+
   p.ttest = as.data.frame(p.ttest)
   p.ttest$p0.05 = ifelse(p.ttest$fdr < 0.05, T, F)
   return(p.ttest)
@@ -471,19 +476,27 @@ ggplot(subset(alleffects,site=='pooled'), aes(y=cohen.D, x=Region)) +
 ggsave("ENIGMAvolumes_horizpplot.pdf", width=9, height=7)
 
 ###################### Now for ENIGMA dti measures
-FAresults <- read.csv("../dti/data/enigmaDTI-FA-results.csv")
+## read in the data
+setwd("/projects/edickie/code/spins-1yr-analysis/dti")
+FAresults <- read.csv("data/enigmaDTI-FA-results.csv")
 FAvars <- names(FAresults)[2:length(names(FAresults))]
 FA_average_col <- grep('Average', FAvars, value = T)
 FA_LandRrois <- c(grep('.L_', FAvars, value = T),grep('.R_', FAvars, value = T))
 PooledRois <- setdiff(FAvars,c(FA_LandRrois,FA_average_col))
 
-ENIGMA_look_up_table <- read.delim("/projects/edickie/code/hcp_extras/templates/ENIGMA_look_up_table.txt", header=FALSE)
-names(ENIGMA_look_up_table) <-c('key','name','X1','description')
+## Ran previously...
+# ENIGMA_look_up_table <- read.delim("/projects/edickie/code/hcp_extras/templates/ENIGMA_look_up_table.txt", header=FALSE)
+# names(ENIGMA_look_up_table) <-c('KEY','NAME','X1','description')
+# engimalut <- ENIGMA_look_up_table[ ,c('KEY',"NAME",'description')]
+#write.csv(engimalut, "ENIGMA_look_up_table.csv", row.names = F)
+## load the engima lookup table
+ENIGMA_look_up_table <- read.csv("skeltemplates/ENIGMA_look_up_table.csv")
+
 
 FAvars_noFA <-gsub('_FA','',FAvars, fixed=T)
 FAvars_noFA <-gsub('.','-',FAvars_noFA, fixed=T)
 
-Skelvars <- intersect(FAvars_noFA,as.character(ENIGMA_look_up_table$name))
+Skelvars <- intersect(FAvars_noFA,as.character(ENIGMA_look_up_table$NAME))
 Skelvars_FAed <- paste0(gsub('-','.',Skelvars, fixed=T),'_FA')
 
 ## merge down the one's from the list
@@ -502,4 +515,54 @@ ttestres_FAresid_pooled = ttestpvals(FAskel.melted,'ROI','FA_resid ~ Dx')
 ttestres_FAresid_pooled$ROI = gsub('_FA','',row.names(ttestres_FAresid_pooled), fixed=T)
 ttestres_FAresid_pooled$ROI = gsub('.','-',ttestres_FAresid_pooled$ROI, fixed=T)
 ttestres_FAresid_pooled <- ttestres_FAresid_pooled[ ,c("ROI",names(ttestres_FAresid_pooled)[1:9])]
-write.csv(ttestres_FAresid_pooled,"ttestres_FAresid_allsites.csv",row.names = F)
+#write.csv(ttestres_FAresid_pooled,"ttestres_FAresid_allsites.csv",row.names = F)
+
+FApooled <- FAresults[ ,c(names(subjects),PooledRois)] 
+FApooled_healthyavg <-cbind(apply(subset(FApooled, Dx == "Control")[ ,c(PooledRois)],2,mean),PooledRois)
+FApooled_healthyavg <- as.data.frame(FApooled_healthyavg)
+FApooled_healthyavg[order(FApooled_healthyavg$V1),]
+PooledRois_horder <- as.character(FApooled_healthyavg[order(FApooled_healthyavg$V1),'PooledRois'])
+
+##melt
+FApooled.melted <- melt(FAresults[,c(names(subjects),PooledRois)],
+                      id.vars=c(names(subjects)),
+                      measure.vars=PooledRois,
+                      variable.name="ROI",
+                      value.name="FA")
+
+ttestres_pFA_raw = ttestpvals(FApooled.melted,'ROI','FA ~ Dx')
+ttestres_pFA_raw$site = 'pooled'
+### do separately in all site and recombine
+ttestres_pFA_raw_CMH = ttestpvals(subset(FApooled.melted,site=='CMH'),'ROI','FA ~ Dx')
+ttestres_pFA_raw_CMH$site = 'CMH'
+ttestres_pFA_raw_ZHH = ttestpvals(subset(FApooled.melted,site=='ZHH'),'ROI','FA ~ Dx')
+ttestres_pFA_raw_ZHH$site = 'ZHH'
+ttestres_pFA_raw_MRC = ttestpvals(subset(FApooled.melted,site=='MRC'),'ROI','FA ~ Dx')
+ttestres_pFA_raw_MRC$site = 'MRC'
+
+alleffects_FA = rbind(ttestres_pFA_raw,ttestres_pFA_raw_CMH, ttestres_pFA_raw_ZHH, ttestres_pFA_raw_MRC)
+
+alleffects_FA$cohen.D <- as.numeric(alleffects_FA$cohen.D)
+alleffects_FA$cohenD.se <- as.numeric(alleffects_FA$cohenD.se)
+
+alleffects_FA$Tract <- NA
+for (i in 1:nrow(alleffects_FA)) {
+  alleffects_FA$Tract[i] <- as.character(strsplit(row.names(alleffects_FA)[i],'_')[[1]][1])
+}
+
+alleffects_FA$cohen.D
+tmp <- subset(alleffects_FA,site=='pooled')
+alleffects_FA_order <- as.character(tmp[order(as.numeric(tmp$cohen.D)),'Tract'])
+alleffects_FA$Tract <- factor(alleffects_FA$Tract,levels = alleffects_FA_order)
+
+alleffects_FA_horder <- gsub('_FA','',PooledRois_horder,fixed=T)
+#alleffects_FA$Tract <- factor(alleffects_FA$Tract,levels = alleffects_FA_horder)
+  
+ggplot(subset(alleffects_FA,site=='pooled'), aes(y=as.numeric(cohen.D), x=Tract)) +
+  geom_errorbar(aes(ymin = (cohen.D - cohenD.se), ymax =  (cohen.D + cohenD.se)), width = 0.2) +
+  geom_point(size = 8, shape = 18) + 
+  geom_point(data=subset(alleffects_FA,site!='pooled'), aes(y=cohen.D, x=Tract, colour = site), size = 3) +
+  geom_hline(yintercept=0) +
+  scale_y_reverse() + 
+  labs(x='',y="Group Effect Size (Hedges g)") +
+  coord_flip()
